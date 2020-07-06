3450764e62a2641d70f1566144dd4c5aede3149e
Pamela Soulis <pamela.soulis@gmail.com>
2020-07-06 17:38:07 -0400

    initial setup

---
+++ pamela-project1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pamela_project1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
